d6bce19260e454536d02f3a4bcaa751afba3ef48
Jorge Castro Vargas <david92_jc@yahoo.es>
2022-10-25 23:19:54 -0500

    Feat: Add health swagger documentation

--- backend/src/routes/health.routes.js
+++ backend/src/routes/health.routes.js
@@ -2,8 +2,28 @@ import { Router } from "express";
 import { getHealth } from "../controllers/health.controller.js";
 
 const router = Router();
-
-
+/// https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/#document-your-api-properly
+/// https://dev.to/kabartolo/how-to-document-an-express-api-with-swagger-ui-and-jsdoc-50do
+/**
+ * @openapi
+ * /api/health:
+ *   get:
+ *     summary: Health Checker
+ *     description: Retrive our service status.
+ *     tags:
+ *       - Health
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: available
+ */
 router.get('/', getHealth);
 
 export default router;
