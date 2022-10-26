e2785a4bb1f5a59e164524b57baff29f502ecf46
jeici21 <114098379+jeici21@users.noreply.github.com>
2022-10-25 23:23:57 -0500

    Feat/api swagger (#4)
    * Feat: install dependencies
    
    * Feat: Add swagger configuration
    
    * Feat: Add health swagger documentation
    
    * Feat: add pokemon swagger documentation
    
    * Feat: Add Types swagger documentation

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import './models/PokemonsEvolve.js';
 
 import app from './app.js';
 import { sequelize } from "./database/config.js";
+import { swaggerDocs } from './swagger.js';
 
 const port = 3000;
 
@@ -13,6 +14,7 @@ const main = async () => {
         await sequelize.sync({ forced: true });
         app.listen(port, () => {
             console.log(`listening port ${port}`);
+            swaggerDocs(app, port);
         });
     } catch (error) {
         console.error('Unable to connect to the database:', error);
